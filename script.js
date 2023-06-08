93136010df7320010b7d73e073be4f16d5f171f7
Naratip <naratip.18257@gmail.com>
2023-06-08 14:51:51 +0700

    create function getComuterChoice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function getComputerChoice(){
+    let rand = Math.floor( Math.random(0,3) ) ;
+    if( rand === 0) return  "Rock";
+    else if( rand === 1) return "Paper";
+    return "Scissors";
+}
