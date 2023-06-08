fad09ba0990f69aaaac7cc26be675cc2e4d84751
Naratip <naratip.18257@gmail.com>
2023-06-08 15:05:18 +0700

    create function play

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function getComputerChoice(){
     else if( rand === 1) return "Paper";
     return "Scissors";
 }
+
+function play(computerSelection,playerSelection){
+    let player = playerSelection.toLowerCase();
+    switch(player){
+        case "rock":
+            if(computerSelection === "Rock") return "Tie!";
+            else if(computerSelection === "Paper") return "You Lose! Paper beats Rock";
+            return "You Win! Rock beats Scissors"
+            break;
+        case "paper":
+            if(computerSelection === "Rock") return "You Win! Paper beats Rock";
+            else if(computerSelection === "Paper") return "Tie!";
+            return "You Lose! Scissors beats Paper"
+            break;
+        case "scissors":
+            if(computerSelection === "Rock") return "You Lose! Rock beats Scissors";
+            else if(computerSelection === "Paper") return "You Win! Scissors beats Paper";
+            return "Tie!"
+            break;
+    }
+}
